70df613e0c3235e26ee3926baa09d3b788027397
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-01-09 17:32:20 +0800

    feat: use link based connection strings for pool

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const { PROD_DATABASE_USER, PROD_PASSWORD, PROD_HOST, PORT, PROD_DATABASE } =
-  process.env;
-
 module.exports = new Pool({
-  connectionString: `postgresql://${PROD_DATABASE_USER}:${PROD_PASSWORD}@${PROD_HOST}:${PORT}/${PROD_DATABASE}`,
+  connectionString: process.env.INTERNAL_DATABASE_URL,
 });
