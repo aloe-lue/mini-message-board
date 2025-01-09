a7832c64524e373510e48465311217d1a52c494f
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-01-09 13:07:38 +0800

    feat: use queries from db to populate views

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,17 +1,42 @@
-const message = require("../messages");
+// const message = require("../messages");
+const db = require("../db/queries");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
-const getMessagesController = async function messagesGetter(req, res) {
-  const data = await message.getMessages();
+const getMessagesController = asyncHandler(async function messagesGetter(
+  req,
+  res
+) {
+  const data = await db.getAllMessages();
+
+  if (!data) {
+    return res.render("indexView", { title: "Mini messageboard", message: [] });
+  }
 
   res.render("indexView", { title: "Mini Messageboard", messages: data });
-};
+});
 
-const setMessageController = async function messageSetter(req, res) {
-  const obj = req.body;
+const messageValidationChain = [
+  body("authorName").trim().notEmpty(),
+  body("messageText").trim().notEmpty(),
+];
 
-  message.setMessage(obj);
+const setMessageController = [
+  messageValidationChain,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  res.redirect("/");
-};
+    if (!errors.isEmpty()) {
+      return res.status(400).render("formView", {
+        title: "mini message board: new",
+        errors: errors.array(),
+      });
+    }
+
+    const { authorName, messageText } = req.body;
+    await db.addMessage(messageText, authorName, new Date());
+    res.redirect("/");
+  }),
+];
 
 module.exports = { getMessagesController, setMessageController };
